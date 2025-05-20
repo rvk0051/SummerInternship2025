7e011e7768fb2a1735062710b20c6b635ba7c22c
Kesar <rvk0051@gmail.com>
2025-05-20 17:28:36 +0530

    Data types, GFG questions and some more python basi concepts

---
+++ week1/day1/MultipleLineStatements.py
@@ -0,0 +1,18 @@
+#Writing a long statement in a code is not feasible or readable.
+#Breaking a long line of code into multiple lines makes is more readable.
+
+#Using backSlash (\)
+#In Python, you can break a statement into multiple lines using the backslash (\).
+# This method is useful, especially when we are working with strings or mathematical operations.
+
+sentence = "This is a very long sentence that we want to " \
+           "split over multiple lines for better readability."
+
+print(sentence)
+
+# For mathematical operations
+total = 1 + 2 + 3 + \
+        4 + 5 + 6 + \
+        7 + 8 + 9
+
+print(total)
