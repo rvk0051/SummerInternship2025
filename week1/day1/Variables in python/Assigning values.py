9b5775fc6db0d3c95ffa8c8acc7bf70d0865db95
Kesar <rvk0051@gmail.com>
2025-05-20 12:38:40 +0530

    Comment, indentation, and some parts of variables in python

---
+++ week1/day1/Variables in python/Assigning values.py
@@ -0,0 +1,16 @@
+# Variables in Python are assigned values using the = operator.
+var1=5 # 5 value is assigned to variable 'var1'
+print(var1)
+
+#Python variables are dynamically typed,
+# the same variable can hold different types of values during execution.
+var1= "Now a string"
+print(var1) # Data type of the variable changed dynamically.
+
+#Assigning variables the same value
+num1 = num2 = num3 = 100 #All the variables store same value
+print(num1, num2, num3)
+
+#Assigning different values to multiple variables simultaneously.
+n1, n2, n3= 1, 2.5, "Python"
+print(n1, n2, n3)
---
+++ week1/day1/Variables in python/Naming Rules.py
@@ -0,0 +1,20 @@
+# Naming Rules for Python
+#1. Variable names can only contain letters, digits and underscores (_).
+#2. A variable name cannot start with a digit.
+#3. Variable names are case-sensitive (abc and aBc are different).
+#4. Avoid using Python keywords (e.g., if, else, for) as variable names.
+
+age=21
+_color= 'Blue'
+total_123='6'
+
+ #Here, all 3 variables defined above are valid
+
+Total_123='9'
+
+# total_123 and Tota;_123 are different variables
+
+# Let's see some examples of invalid variables
+# 1name='abc' # invalid as it starts with a digit
+# class = 10 # invalid as class is a reserved keyword
+# user-name ="Doe" # invalid as it contains hyphen
