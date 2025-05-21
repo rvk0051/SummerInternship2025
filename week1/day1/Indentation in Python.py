2293e5a2b5db4ceeca3adaa4f050f2fe083f25cb
Kesar <rvk0051@gmail.com>
2025-05-21 08:32:13 +0530

    identifiers, indentation and time-space complexity added

---
+++ week1/day1/Indentation in Python.py
@@ -0,0 +1,18 @@
+# Indentation refers to the spaces at the beginning of a code line.
+# Python uses indentation to indicate a block of code.
+
+#Example:-
+if 5 > 2:
+  print("Five is greater than two!")
+
+#Python will give you an error if you skip the indentation:
+#if 5 > 2:
+#print("Five is greater than two!") #This shows error
+
+#The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
+if 5 > 2:
+ print("Five is greater than two!")
+if 5 > 2:
+        print("Five is greater than two!")
+
+#You have to use the same number of spaces in the same block of code, otherwise Python will give an error
