cde4478aedd817a73e9d8d39f173f044430419b0
Kesar <rvk0051@gmail.com>
2025-05-21 17:37:33 +0530

    Read about gitignore

---
+++ week1/day2/about gitignore/Intro.py
@@ -0,0 +1,12 @@
+#The .gitignore file tells Git which files and folders to ignore (not track).
+
+#This is useful for keeping log files, temporary files, build artifacts, or personal files out of your repository.
+
+#Examples of files to ignore: log files, temporary files, hidden files, personal files, OS/editor files, etc.
+#The .gitignore file itself is tracked by Git, so everyone using the repository ignores the same files.
+
+
+#When to Use .gitignore :-
+# 1. When you want to keep sensitive, local, or unnecessary files out of your repository
+# 2. When sharing a project with others and want to avoid cluttering Git history
+# 3. When working with build tools or editors that create extra files
