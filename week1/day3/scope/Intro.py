b7506f6078f1db7c0ec274fd3bd1f568198eb529
Kesar <rvk0051@gmail.com>
2025-05-22 14:58:04 +0530

    Changes in comments

--- week1/day3/scope/Intro.py
+++ week1/day3/scope/Intro.py
@@ -1,6 +1,7 @@
 # Scope in Python
 
-#Scope refers to the region of a program where a name (identifier) is recognized. If a name is not in the current scope, Python will search for it in outer scopes using the LEGB rule.
+#Scope refers to the region of a program where a name (identifier) is recognized.
+# If a name is not in the current scope, Python will search for it in outer scopes using the LEGB rule.
 
 # 1. Local (L): Names assigned within a function.
 # 2. Enclosing (E): Names in the local scope of enclosing functions (for nested functions).
--- week1/day3/scope/built_in_scope.py
+++ week1/day3/scope/built_in_scope.py
@@ -1,7 +1,8 @@
 # Built-in Scope
 
-#The built-in scope is the outermost and widest scope in Python.
-#It contains all the standard functions and exceptions that are automatically available in every Python program — without importing anything.
+# The built-in scope is the outermost and widest scope in Python.
+# It contains all the standard functions and exceptions
+# that are automatically available in every Python program — without importing anything.
 
 #These names live in the builtins module.
 #You don’t need to import it to use its functions (like print, len, range, etc.).
