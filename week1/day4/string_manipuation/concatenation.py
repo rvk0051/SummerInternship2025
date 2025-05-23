6c3542874812ad9f059731a5de94e441033e7748
Kesar <rvk0051@gmail.com>
2025-05-23 10:56:37 +0530

    Adding topic string manipulation

---
+++ week1/day4/string_manipuation/concatenation.py
@@ -0,0 +1,8 @@
+# Concatenation means to join two or more strings into a single string.
+# + operator is used to concatenate strings. For example,
+
+# Example:
+word1 = 'In Time'
+word2 = ' Tec'
+print(word1 + word2)  # concatenating strings 'In Time' and 'Tec' using + operator
+# Output: In Time Tec
---
+++ week1/day4/string_manipuation/replace.py
@@ -0,0 +1,8 @@
+# replace() method
+
+# The replace() method is used to replace a part of the string with another string.
+
+# As an example, let's replace 'ing' in Coding with 'ers,' making it Coders.
+string = 'Coding'
+string = string.replace('ing', 'ers')
+print(string) # output: Coders
