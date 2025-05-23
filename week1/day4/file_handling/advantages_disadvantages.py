b5bf64b5c1d7b0ba997c46e22bd2c7d93dac8eac
Kesar <rvk0051@gmail.com>
2025-05-23 09:38:44 +0530

    file handling done

---
+++ week1/day4/file_handling/advantages_disadvantages.py
@@ -0,0 +1,27 @@
+'''
+Advantages of File Handling in Python
+
+1. Versatility : File handling in Python allows us to perform a wide range of operations,
+                 such as creating, reading, writing, appending, renaming and deleting files.
+2. Flexibility : File handling in Python is highly flexible, as it allows us to work with different file types
+                 (e.g. text files, binary files, CSV files , etc.) and
+                 to perform different operations on files (e.g. read, write, append, etc.).
+3. User - friendly : Python provides a user-friendly interface for file handling,
+                     making it easy to create, read and manipulate files.
+4. Cross-platform : Python file-handling functions work across different platforms (e.g. Windows, Mac, Linux),
+                    allowing for seamless integration and compatibility.
+
+Disadvantages of File Handling in Python
+
+1. Error-prone: File handling operations in Python can be prone to errors,
+                especially if the code is not carefully written or if there are issues with the file system
+                (e.g. file permissions, file locks, etc.).
+2. Security risks : File handling in Python can also pose security risks,
+                    especially if the program accepts user input that can be used to access
+                    or modify sensitive files on the system.
+3. Complexity : File handling in Python can be complex,
+                especially when working with more advanced file formats or operations.
+                Careful attention must be paid to the code to ensure that files are handled properly and securely.
+4. Performance : File handling operations in Python can be slower than other programming languages,
+                 especially when dealing with large files or performing complex operations.
+'''
