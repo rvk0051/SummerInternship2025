e44d405831ae35e7bf7446b162b2a10523314a06
Kesar <rvk0051@gmail.com>
2025-05-27 11:57:42 +0530

    polymorphism

---
+++ week2/day2/polymorphism/intro.py
@@ -0,0 +1,18 @@
+'''
+
+Polymorphism in python:-
+
+Polymorphism is a foundational concept in programming that allows
+entities like functions, methods or operators to behave differently based on the type of data they are handling.
+Derived from Greek, the term literally means "many forms".
+
+
+Table of Contents:-
+
+1. Polymorphism in Built-in Functions
+2. Polymorphism in Functions
+3. Polymorphism in Operators
+4. Polymorphism in Object-Oriented Programming (OOP)
+5. Types of Polymorphism
+
+'''
